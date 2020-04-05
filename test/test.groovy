4d43bb9ed7a2a018092afdef6f1120e29ad10a7a
Darian Campos <darian.campos@pedidosya.com>
2020-04-05 20:13:21 -0300

    update

--- test/test.groovy
+++ test/test.groovy
@@ -4,7 +4,25 @@ pipeline {
     //add environment
     environment {
        appname = "${APPNAME}"
+       tag = "${TAG}"
+       env_credential = "dariancampospeya"
     }
+
+    stages {
+        stage('checkout'){
+            steps {
+                dir("${appname}"){
+                    checkout([$class: 'GitSCM',
+                    branches: [[name: "${tag}"]],
+                    extensions: [[$class: 'CloneOption', timeout: 120]],
+                    gitTool: 'Default',
+                    userRemoteConfigs: [[url: "git@github.com:dariancampospeya/test.git", credentialsId: "${env_credential}"]]
+                    ])
+                }         
+            }
+        }
+
+        {% include '${appname}/_preparation.groovy' with context %}
 
         
         //inject stage code
